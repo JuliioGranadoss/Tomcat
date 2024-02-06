412efe3c04c0fb3eeb5432bc19c525ef2d15e9ee
Julio Granados Duran <118803823+JuliioGranadoss@users.noreply.github.com>
2024-02-06 08:56:03 +0100

    Add files via upload

--- stack.sh
+++ stack.sh
@@ -2,7 +2,13 @@
 # Nombre del stack
 STACK_NAME=web-app-stack
 # Ruta del archivo de la plantilla
-TEMPLATE_FILE=main.yml
+TEMPLATE_FILE=ubuntu.yml
 
 # Desplegar el stack
 aws cloudformation deploy --stack-name $STACK_NAME --template-file $TEMPLATE_FILE --capabilities CAPABILITY_IAM
+
+# Obtener la URL de la aplicación
+if [ $? -eq 0 ]; then
+    aws cloudformation list-exports \
+        --query "Exports[?Name=='IPaddress'].Value"
+fi
